fa5c6a6e56fff3e648535cd2f257059118634180
Wilson Mbuthia <wyllyjeremy@gmail.com>
2023-03-03 10:12:39 +0300

    commits

--- wp-content/themes/assessment/functions.php
+++ wp-content/themes/assessment/functions.php
@@ -6,6 +6,8 @@
 function custom_enqueue_styles(){
     wp_register_style('customstyle', get_template_directory_uri(). '/css/custom.css', array(), '1.0.0', 'all');
     wp_enqueue_style('customstyle');
+    wp_register_style('four', get_template_directory_uri(). '/css/four.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('four');
 }
 add_action('wp_enqueue_scripts', 'custom_enqueue_styles');
 /*-------------------------------------------------------------------------*/
@@ -59,3 +61,73 @@ add_theme_support('post-formats',array('aside','image','video'));
 
 add_theme_support('custom-header');
 
+/*-------------------------------------------------------------------------*/
+/*                        LIMIT LOGIN ATTEMPTS                             */
+/*-------------------------------------------------------------------------*/
+function check_attempted_login( $user, $username, $password ) {
+    if ( get_transient( 'attempted_login' ) ) {
+        $datas = get_transient( 'attempted_login' );
+
+        if ( $datas['tried'] >= 5 ) {
+            $until = get_option( '_transient_timeout_' . 'attempted_login' );
+            $time = time_to_go( $until );
+
+            return new WP_Error( 'too_many_tried',  sprintf( __( '<strong>ERROR</strong>: You have reached authentication limit, you will be able to try again in %1$s.' ) , $time ) );
+        }
+    }
+
+    return $user;
+}
+
+add_filter( 'authenticate', 'check_attempted_login', 30, 3 ); 
+
+function login_failed( $username ) {
+    if ( get_transient( 'attempted_login' ) ) {
+        $datas = get_transient( 'attempted_login' );
+        $datas['tried']++;
+
+        if ( $datas['tried'] <= 3 )
+            set_transient( 'attempted_login', $datas , 300 );
+    } else {
+        $datas = array(
+            'tried'     => 1
+        );
+        set_transient( 'attempted_login', $datas , 300 );
+    }
+}
+
+add_action( 'wp_login_failed', 'login_failed', 10, 1 ); 
+
+function time_to_go($timestamp){
+
+    // converting the mysql timestamp to php time
+    $periods = array(
+        "second",
+        "minute",
+        "hour",
+        "day",
+        "week",
+        "month",
+        "year"
+    );
+    $lengths = array(
+        "60",
+        "60",
+        "24",
+        "7",
+        "4.35",
+        "12"
+    );
+    $current_timestamp = time();
+    $difference = abs($current_timestamp - $timestamp);
+    for ($i = 0; $difference >= $lengths[$i] && $i < count($lengths) - 1; $i ++) {
+        $difference /= $lengths[$i];
+    }
+    $difference = round($difference);
+    if (isset($difference)) {
+        if ($difference != 1)
+            $periods[$i] .= "s";
+            $output = "$difference $periods[$i]";
+            return $output;
+    }
+}
